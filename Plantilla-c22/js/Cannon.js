d84ed719eb6ff8d453f58a712822162920bf5072
Daniel <105463376+DNC2022@users.noreply.github.com>
2022-07-05 17:50:47 -0500

    Add files via upload

---
+++ Plantilla-c22/js/Cannon.js
@@ -0,0 +1,25 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannon_image = loadImage("assets/canon.png");
+    this.cannon_base = loadImage("assets/cannonBase.png");
+  }
+
+  //codigo para crear la parte superior de el cañon
+  display() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    imageMode(CENTER);
+    image(this.cannon_image, 0, 0, this.width, this.height);
+    pop();
+
+    //codigo para crear la parte inferior del cañon
+    image(this.cannon_base,70, 20, 200, 200);
+    noFill();
+  }
+}
